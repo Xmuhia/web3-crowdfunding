5d9570f0ba5ca5da3dda6733a68bf01f3d5ed0e5
Brendan Muhia <dotcombrendan@gmail.com>
2025-10-04 22:51:50 +0300

    fix deploy script for CrowdFunding contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,10 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
-  const crowdFunding = await CrowdFunding.deploy(unlockTime, { value: lockedAmount });
+  const crowdFunding = await CrowdFunding.deploy();
 
   await crowdFunding.deployed();
 

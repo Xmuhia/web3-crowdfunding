b707b863d958faa5b114cf8a6833c186a3906b12
Brendan <dotcombrendan@gmail.com>
2025-10-20 03:48:48 +0300

    implement createCampaign function with Web3Modal wallet connection and contract interaction

--- context/CrowdFunding.js
+++ context/CrowdFunding.js
@@ -15,4 +15,29 @@ export const CrowdFundingProvider = ({children}) => {
     const titleData = 'Crowd Funding Contract';
     const [currentAccount, setCurrentAccount] = useState('');
 
+    const createCampaign = async (campaign) => {
+        const {title, description, amount, deadline} = campaign;
+        const Web3Modal = new Web3Modal();
+        const connection = await Web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = fetchContract(signer)
+
+        console.log(currentAccount);
+        try{
+            const transaction = await contract.createCampaign(
+                currentAccount, //owner
+                title,
+                description,
+                ethers.utils.parseUnits(amount, 18),
+                new Date(deadline).getTime()//
+            );
+
+            await transaction.wait();
+
+            console.log('contract call success', transaction)
+        } catch (error) {
+            console.log('contract call failure', error);
+        }
+    }
 }

a52f4b56bfc54aa6a94e305f4ba9d51f66abb76c
Brendan <dotcombrendan@gmail.com>
2025-10-20 04:03:17 +0300

    add getCampaign function to fetch and parse all campaigns from blockchain

--- context/CrowdFunding.js
+++ context/CrowdFunding.js
@@ -39,5 +39,24 @@ export const CrowdFundingProvider = ({children}) => {
         } catch (error) {
             console.log('contract call failure', error);
         }
+    };
+
+    const getCampaign = async () => {
+        const provider = new ethers.providers.JsonRpcProvider();
+        const contract = fetchContract(provider);
+
+        const campaigns = await contract.getCampaigns();
+        const parsedCampaigns = campaigns.map((campaign, i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(
+                campaign.amountCollected.toString()
+            ),
+            pId: i,
+        }))
+        return parsedCampaigns;
     }
 }
